fb6973bc7c13d0ea305ca7d9910e9e9393dbaa87
栩 张 <1105806102@qq.com>
2021-01-10 17:41:13 +0800

    初始化

---
+++ untitled/src/Studay01.java
@@ -0,0 +1,14 @@
+public class Studay01 {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("Studay01.main");
+        Circle c1= new Circle(2.3);
+        Circle c2=new Circle("white",2.3,2);
+
+        System.out.println(c1.getColor().equals(c2.getColor()));
+        System.out.println(c1.);
+    }
+}
